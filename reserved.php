908ac4644e0d8a2d7c5f081e3d5c7fe93a453390
Eric <eric@carmelsoft.com>
2013-06-17 14:13:32 -0700

    Initial commit.

---
+++ reserved.php
@@ -0,0 +1,50 @@
+E_USER_ERROR
+abstract $
+and
+as
+break
+case
+catch
+class
+const a
+continue
+echo
+else
+endif
+exit
+extends
+false
+final a
+for
+foreach
+function a()
+global
+if
+implements a
+include
+include_once
+instanceof
+interface
+new a
+null
+or
+parent
+print
+private
+protected $
+public $
+require
+require_once
+return
+static
+switch
+throw
+true
+try
+var
+while{};
+$_GET
+$_POST
+$_SERVER
+$_SESSION
+$this
